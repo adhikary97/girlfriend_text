059f2119df52d26bc6a3f9c0492968d960ffc380
HelixAchaos <peter.e.chon@gmail.com>
2021-03-11 18:38:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import datetime
-import time as t
-import re
 import subprocess
 import util
 
 
 def send_message():
-    pattern = r'08:\d{2}:\d{2}'
     while True:
-        time = datetime.datetime.now().strftime('%H:%M:%S')
-        match = re.match(pattern, time)
-        if match:
+        hour = datetime.datetime.now().strftime('%H')
+        if hour == '08':
             subprocess.call(f"bash runner.bash '{util.phone_number}' '{util.message}'", shell=True)
-            t.sleep(86400)
+            time.sleep(86400)
 
 
 if __name__ == '__main__':
