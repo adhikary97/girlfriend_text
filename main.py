df4ec151ceeb0157ec7046fa8f8649040e490a23
HelixAchaos <peter.e.chon@gmail.com>
2021-03-11 18:36:15 -0500

    Update main.py
    replaced %s with an f-string. also replaced [0-9] with \d. 
    
    one can use r'8(?::\d{2}){2}', but it's longer. it also takes a bit more time to read (ie. it's less readable).

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import util
 
 
 def send_message():
-    pattern = r'(08):[0-9]{2}:[0-9]{2}'
+    pattern = r'08:\d{2}:\d{2}'
     while True:
         time = datetime.datetime.now().strftime('%H:%M:%S')
         match = re.match(pattern, time)
         if match:
-            subprocess.call("bash runner.bash '%s' '%s'" % (util.phone_number, 'Good Morning Babe'), shell=True)
+            subprocess.call(f"bash runner.bash '{util.phone_number}' '{util.message}'", shell=True)
             t.sleep(86400)
 
 
